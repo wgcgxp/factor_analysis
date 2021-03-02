aeb8ca5402481e63b2a64c9ab472aa441a3efecf
zhangyang <zjsxzy@gmail.com>
2021-03-02 15:28:33 +0800

    version 0.1

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name="factor_analysis",
+        version="0.1",
+        author="zhangyang",
+        packages=find_packages()
+    )
---
+++ test/test_utils.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+import factor_analysis as fa
+
+fa.utils.hello()
